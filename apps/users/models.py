edeff002dc06f3c3f05de9005d9df4ed057f7ffd
hupingan86 <406839815@qq.com>
2019-08-07 13:59:23 +0800

    Initial commit

---
+++ apps/users/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+
+
+# Create your models here.
+class UserProfile(AbstractUser):
+    """
+    用户信息
+    """
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
+    namephoto = models.ImageField(upload_to="goods/images/", null=True, blank=True, verbose_name="用户头像")
+    lianxiren = models.CharField(max_length=30, default="", verbose_name="联系人")
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+class VerifyCode(models.Model):
+    """
+    短信验证码
+    """
+    code = models.CharField(max_length=10, verbose_name="验证码")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "短信验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.code
